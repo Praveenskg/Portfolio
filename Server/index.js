4e122d1d92156cf96d6b9316880e68f6befe0070
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-10 23:21:12 +0530

    ✅ Added Cors Policy

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,19 @@
 import "dotenv/config";
 import express from "express";
 const app = express();
+import cors from "cors";
 import authRoute from "./router/auth-route.js";
 import contactRoute from "./router/contact-route.js";
 import connectDb from "./utils/db.js";
 import errorMiddleware from "./middleware/error-middleware.js";
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET ,POST ,PUT ,DELETE, PATCH , HEAD",
+  Credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
