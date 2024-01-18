d5af5bf7b959be683a9657c5afb960449d5713f0
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-18 23:29:29 +0530

    🎨 Added Auth Middleware

---
+++ Server/middleware/auth-middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user-model.js";
+const authMiddleware = async (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: " Unauthorized HTTP , Token not Provided" });
+  }
+  const jwtToken = token.replace("Bearer", "").trim();
+  console.log("Token from auth middleware ", jwtToken);
+  try {
+    const isVerified = jwt.verify(jwtToken, process.env.JWT_SECRETE_KEY);
+
+    const userData = await User.findOne({ email: isVerified.email }).select({
+      password: 0,
+    });
+    console.log(userData);
+    req.user = userData;
+    req.token = token;
+    req.userId = userData._id;
+    next();
+  } catch (error) {}
+};
+export default authMiddleware;
