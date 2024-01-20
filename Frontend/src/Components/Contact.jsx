e76ad733729fa28af2b75585770049b1cb919c1b
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-20 22:30:52 +0530

    ✅  contact form connected with server

--- Frontend/src/Components/Contact.jsx
+++ Frontend/src/Components/Contact.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "animate.css/animate.min.css";
+import { useAuth } from "../store/auth";
 
+const defaultContact = {
+  username: "",
+  email: "",
+  phone: "",
+  message: "",
+};
 const Contact = () => {
+  const [contact, setContact] = useState(defaultContact);
+  const [userData, setUserData] = useState(true);
+  const { user } = useAuth();
+
+  if (userData && user) {
+    setContact({
+      username: user.username,
+      email: user.email,
+      phone: user.phone,
+      message: "",
+    });
+    setUserData(false);
+  }
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/form/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(contact),
+      });
+      if (response.ok) {
+        setContact(defaultContact);
+        alert("message send successfully");
+      }
+    } catch (error) {
+      alert("message not send");
+      console.log(error);
+    }
+  };
   return (
     <>
       <section className="bg-white dark:bg-gray-800 capitalize " id="contact">
@@ -12,22 +60,23 @@ const Contact = () => {
           <p className="mb-4 lg:mb-8 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
             I would love to hear from you.
           </p>
-          <form className="space-y-8">
+          <form className="space-y-8" onSubmit={handleSubmit}>
             <div>
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Your Name
               </label>
               <input
-                type="email"
-                id="email"
+                type="text"
+                id="username"
+                name="username"
+                onChange={handleInput}
+                required
+                autoComplete="off"
+                value={contact.username}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Full Name"
-                required
               />
-            </div>{" "}
+            </div>
             <div>
               <label
                 htmlFor="email"
@@ -38,22 +87,25 @@ const Contact = () => {
               <input
                 type="email"
                 id="email"
+                name="email"
+                onChange={handleInput}
+                value={contact.email}
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@domain.com"
                 required
               />
             </div>
             <div>
-              <label
-                htmlFor="subject"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Phone
               </label>
               <input
-                type="text"
-                id="subject"
-                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                type="number"
+                id="phone"
+                name="phone"
+                onChange={handleInput}
+                value={contact.phone}
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500  appearance-none focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Phone Number  "
                 required
               />
@@ -67,10 +119,12 @@ const Contact = () => {
               </label>
               <textarea
                 id="message"
+                name="message"
+                onChange={handleInput}
                 rows={6}
+                value={contact.message}
                 className="block p-2.5 w-full resize-none text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
-                defaultValue={""}
               />
             </div>
             <button
