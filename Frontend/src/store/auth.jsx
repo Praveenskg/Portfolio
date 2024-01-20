4dea6936c11956aeea4bb92a39df5256ec6c76ee
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-20 22:31:47 +0530

    ♻️ user authentication

--- Frontend/src/store/auth.jsx
+++ Frontend/src/store/auth.jsx
@@ -1,9 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(() => localStorage.getItem("token"));
+  const [user, setUser] = useState("");
 
   const StoreToken = (serverToken) => {
     setToken(serverToken);
@@ -17,8 +18,29 @@ export const AuthProvider = ({ children }) => {
 
   const isLoggedIn = !!token;
 
+  const userAuthentication = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/user", {
+        methods: "GET",
+        headers: {
+          Authorization: `Bearer${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data.userData);
+      }
+    } catch (error) {
+      console.log(`Error Fetching user data `, error);
+    }
+  };
+
+  useEffect(() => {
+    userAuthentication();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ StoreToken, Logout, isLoggedIn }}>
+    <AuthContext.Provider value={{ StoreToken, Logout, isLoggedIn, user }}>
       {children}
     </AuthContext.Provider>
   );
