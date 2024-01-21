920cf6b0b1e6e1887f849d3b421355d0496013db
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-21 23:41:09 +0530

    ✅ Admin Route

--- Server/router/admin-router.js
+++ Server/router/admin-router.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
 import adminController from "../controllers/admin-controller.js";
-router.route("/users").get(adminController.getAllUsers);
-router.route("/contacts").get(adminController.getContact);
+import authMiddleware from "../middleware/auth-middleware.js";
+
+router.route("/users").get(authMiddleware, adminController.getAllUsers);
+router.route("/contacts").get(authMiddleware, adminController.getContact);
 
 export default router;
