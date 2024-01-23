b85f94e902e15a1a1c4bb53ae9cbb5587ed98f86
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-23 23:35:36 +0530

    ⚡️ Created Admin route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import Login from "./Components/Login";
 import Footer from "./Components/Footer";
 import Register from "./Components/Register";
+import Admin from "./Components/Admin/Admin";
+import AdminContacts from "./Components/Admin/AdminContacts";
+import AdminUsers from "./Components/Admin/AdminUsers";
 const App = () => {
   return (
     <>
@@ -14,6 +16,10 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<Admin />}>
+            <Route path="contacts" element={<AdminContacts />} />
+            <Route path="users" element={<AdminUsers />} />
+          </Route>
         </Routes>
         <Footer />
       </BrowserRouter>
