6ea64d71999e3d703978d91f23641f15db7f5259
Praveen Singh <Praveensinghiics@gmail.com>
2024-01-24 23:54:22 +0530

    ✨ Update Data Page

---
+++ Frontend/src/Components/Admin/AdminUpdate.jsx
@@ -0,0 +1,148 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useAuth } from "../../store/auth";
+import { toast } from "react-toastify";
+
+function AdminUpdate() {
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+  });
+  const navigate = useNavigate();
+  const params = useParams();
+  const { authorizationToken } = useAuth();
+
+  const getSingleUser = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/admin/users/${params.id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: authorizationToken,
+          },
+        }
+      );
+      const data = await response.json();
+      console.log("Data received:", data);
+      setData(data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getSingleUser();
+  }, [authorizationToken]);
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/admin/users/update/${params.id}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: authorizationToken,
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        toast.success("Updated Successfully");
+        setTimeout(() => {
+          navigate("/admin/users");
+        }, 2000);
+      } else {
+        toast.error("Not Updated");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-full max-w-sm p-6 m-auto mx-auto bg-white rounded-lg shadow-md dark:bg-gray-900">
+          <div className="flex justify-center mx-auto">
+            <h2 className="text-3xl">Update User Data</h2>
+          </div>
+          <form className="mt-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm text-gray-800 dark:text-gray-200"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={data.username}
+                onChange={handleInput}
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mt-4">
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="email"
+                  className="block text-sm text-gray-800 dark:text-gray-200"
+                >
+                  Email
+                </label>
+              </div>
+              <input
+                type="email"
+                name="email"
+                value={data.email}
+                onChange={handleInput}
+                id="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mt-4">
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="phone"
+                  className="block text-sm text-gray-800 dark:text-gray-200"
+                >
+                  Phone
+                </label>
+              </div>
+              <input
+                type="number"
+                value={data.phone}
+                onChange={handleInput}
+                name="phone"
+                id="phone"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="w-full px-6 py-2.5 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-indigo-500 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+              >
+                Update
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+export default AdminUpdate;
